66039b5b09ddf5a4895909b00a7ff042d122d63a
dkononov <estetis21@gmail.com>
2020-08-12 00:58:01 +0400

    #1 изменил строку вывода

--- src/main/java/ru/aivik/marketdata/service/Application.java
+++ src/main/java/ru/aivik/marketdata/service/Application.java
@@ -3,7 +3,7 @@ package ru.aivik.marketdata.service;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("hello!");
+        System.out.println("hello world!!!");
     }
 
 }
